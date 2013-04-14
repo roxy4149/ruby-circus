e16ba5f6543bd7688b7c9f8377a3e9826096856a
Laila Zaki <laila.circus@gmail.com>
2013-04-14 16:10:52 -0400

    Homework
    working on each method- need to put in all states

--- state.rb
+++ state.rb
@@ -1,11 +1,21 @@
 class State
 	@@states = {"GA" => "Georgia", "CA" => "California"}
 	@@abbreviations = {"Georgia" => "GA", "California" => "CA"}
+	@@state_alpha = ["Georgia", "California", "Alabama", "Flordia"]
+	@@ab_alpha = ["GA", "CA", "AL", "FL"]
 	def name (ab_input)
 		return @@states[ab_input]
 	end
 
 	def abbreviation (state_input)
 		return @@abbreviations[state_input]
+	end
+
+	def all 
+		return @@state_alpha.sort
+	end
+
+	def all_abbreviations
+		return @@ab_alpha.sort
 	end
 end

6aeb977c5501d91f0aeafad72f98dac0fa4da6ea
Laila Zaki <laila.circus@gmail.com>
2013-04-14 16:00:44 -0400

    Homework
    Testing homework- not complete

--- state.rb
+++ state.rb
@@ -1,3 +1,11 @@
-4.times do |variable|
-	puts "hello"
+class State
+	@@states = {"GA" => "Georgia", "CA" => "California"}
+	@@abbreviations = {"Georgia" => "GA", "California" => "CA"}
+	def name (ab_input)
+		return @@states[ab_input]
+	end
+
+	def abbreviation (state_input)
+		return @@abbreviations[state_input]
+	end
 end
